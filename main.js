cb6b667f8b36b71f4bda4eb1894ae3034bcc38b7
Ljilja Mojsilovic <ljmojso@gmail.com>
2021-03-17 13:47:44 +0100

    update and change on main.js file

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ var searchButton = document.querySelector(".search button");
 var videoPreview = document.querySelector(".video-preview");
 
 function onSearch() {
-
   search.value.trim() && getData(search.value);
   search.value = "";
 }
@@ -52,6 +51,17 @@ function createVideo(video) {
     videoPreview.setAttribute('src','https://www.youtube.com/embed/' + video.id.videoId);
   }) 
   videoList.appendChild(container);
+
+  textWrapper.querySelectorAll("h3","image").forEach(function(element){
+    element.addEventListener('click', function() {
+			openVideo(video.id.videoId)
+		});
+	});
+  textWrapper.querySelectorAll("h3","image").forEach(function(element){
+    element.addEventListener('click', function() {
+			getRelatedVideos(video.id.videoId)
+		});
+	});
 }
 
 
@@ -62,18 +72,21 @@ function getRelatedVideos(id) {
 
   relatedVideosRequest.open(
     "GET",
-    " https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&relatedToVideoId=" +
+    " https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&relatedToVideoId=" +
       id +
       "&type=video&key=" +
       key
   );
 
   relatedVideosRequest.onload = function() {
-    listVideos(JSON.parse(relatedVideosRequest.responseText).items);
+    listVideos(JSON.parse(relatedVideosRequest.responseText));
   };
 
   relatedVideosRequest.send();
 }
-
+function openVideo(id) {
+	videoPreview.innerHTML = '<iframe width="100%" src="https://www.youtube.com/embed/' + id + 
+	'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+};
 searchButton.addEventListener("click", onSearch);
 
